4d79bbc5f288e3034d81b51c6f436fbf7e6c66b8
ilyassatlassi <atlassiilyass@gmail.com>
2025-05-07 03:33:16 +0100

    add some

---
+++ logic/dom/render.js
@@ -0,0 +1,6 @@
+function render(element, container) {
+  // TODO create dom nodes
+ const dom = document.createElement(element.type)
+​
+  container.appendChild(dom)
+}
--- src/test.js
+++ src/test.js
@@ -1,34 +1,61 @@
-function test() {
-  const test = document.getElementById("test");
-  const p = document.createElement("p");
-  p.className = "paragraph";
-  p.textContent = `Lorem ipsum dolor sit amet consectetur adipiscing elit
-    . Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placer
-    at in id. Placerat in id cursus mi pretium tellus duis. Pretium 
-    tellus duis convallis tempus leo eu aenean.`;
-  test.appendChild(p);
+
+const container = document.getElementById("root")
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
 }
 
-test();
-
-// test 2
-import { mfCreateElement } from "../logic/dom/creatElement.js";
-
-const vdom = {
-  tag: 'div',
-  attrs: { id: 'root' },
-  children: [
-    { tag: 'h1', attrs: {}, children: ['Hello World'] },
-    { tag: 'button', attrs: { id: 'clickMe' }, children: ['Click me'] }
-  ]
-};
-
-function test2(){
-    const test = document.getElementById("test");
-    const p2 = mfCreateElement(vdom)
-    console.log(p2); 
-    test.appendChild(p2)
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
 }
 
-test2()
+function render(element, container) {
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type)
+  element.props.children.forEach(child =>
+    render(child, dom)
+  )
+  const isProperty = key => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
 
+  container.appendChild(dom)
+}
+
+const ourFrame = {
+  createElement,
+  render
+}
+
+const element = ourFrame.createElement(
+  "div",
+  { id: "foo" },
+  ourFrame.createElement("a", null, "bar"),
+  ourFrame.createElement("b")
+)
+
+
+console.log(element);
+
+ourFrame.render(element, container)
