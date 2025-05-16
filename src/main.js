5552e95899e6314d83d2fe8b966e42e4c50a2f3f
ilyassatlassi <atlassiilyass@gmail.com>
2025-05-16 18:20:38 +0100

    refactor

--- src/main.js
+++ src/main.js
@@ -4,33 +4,6 @@ import { patch } from "./vdom/patch.js";
 
 const container = document.getElementById("root")
 
-let currentState = [];
-let stateIndex = 0;
-let oldVDOM = null;
-
-function rerender() {
-  stateIndex = 0;
-  const newVDOM = App();
-  const patches = diff(oldVDOM, newVDOM);
-  patch(container.firstChild, patches);
-  oldVDOM = newVDOM;
-}
-
-function useState(initialValue) {
-  const localIndex = stateIndex;
-  currentState[localIndex] = currentState[localIndex] !== undefined ? currentState[localIndex] : initialValue;
-  
-  function setState(newValue) {
-    currentState[localIndex] = newValue;
-    console.log("test");
-    rerender();
-  }
-
-  stateIndex++;
-  return [currentState[localIndex], setState];
-}
-
-
 function createElement(type, attrs, ...children) {
   return {
     type,
@@ -60,9 +33,6 @@ function render(element, container) {
     element.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(element.type)
-  element.props.children.forEach(child =>
-    render(child, dom)
-  )
   const isProperty = key => key !== "children"
   Object.keys(element.props)
     .filter(isProperty)
@@ -70,12 +40,16 @@ function render(element, container) {
       if (name.startsWith("on") && typeof element.props[name] === "function") {
         // Attach event listener
         const eventType = name.slice(2).toLowerCase();
-        
+
         dom.addEventListener(eventType, element.props[name]);
       } else {
         dom[name] = element.props[name];
       }
     })
+
+  element.props.children.forEach(child =>
+    render(child, dom)
+  )
 
   container.appendChild(dom)
 }
@@ -85,32 +59,13 @@ const ourFrame = {
   render
 }
 
-// function Header() {
-//   return ourFrame.createElement(
-//     "header",
-//     { class: "header" },
-//     ourFrame.createElement("h1", { class: "header" }, "todos")
-//   );
-// }
+const element = ourFrame.createElement(
+  "div",
+  { id: "foo" },
+  ourFrame.createElement("a", null, "bar"),
+  ourFrame.createElement("b")
+)
 
-function App() {
-  const [count, setCount] = useState(0);
-// console.log(count);
-
-  return ourFrame.createElement(
-    "div",
-    null,
-    ourFrame.createElement("h1", null, "Count: " + count),
-    ourFrame.createElement(
-      "button",
-      { onclick: () => setCount(count + 1) },
-      "Increment"
-    )
-  );
-}
+ourFrame.render(element, container)
 
 
-
-// Initial render with VDOM tracking
-oldVDOM = App();
-ourFrame.render(oldVDOM, container);
