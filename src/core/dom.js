d5261882be4b331c79d1a54301518db163e50394
ynaitedd <bobies431@gmail.com>
2025-05-20 13:56:22 +0100

    update structe && update create elementes

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,19 +1,30 @@
 export function mfCreateElement(vnode) {
-    const el = document.createElement(vnode.tag);
-  
-    // Set attributes
+  const el = document.createElement(vnode.tag);
+
+  // Set attributes
+  if (vnode.attrs) {
     for (const [key, value] of Object.entries(vnode.attrs || {})) {
       el.setAttribute(key, value);
     }
-  
-    // Process children
+  }
+
+  // process events
+  if (vnode.events) {
+    Object.entries(vnode.events).forEach(([event, handler]) => {
+      el[`on${event}`] = handler;
+    });
+  }
+
+  // Process children
+  if (vnode.children) {
     (vnode.children || []).forEach(child => {
       const childEl = typeof child === 'string'
         ? document.createTextNode(child)
         : mfCreateElement(child);
       el.appendChild(childEl);
     });
-  
-    return el;
   }
-  
+
+
+  return el;
+}
