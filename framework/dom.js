1c914deffd78fc78a10afaef43fbe165ae47c9d0
yrahhaou <yassinerahhaoui12@gmail.com>
2025-05-24 07:47:19 +0100

    create changed function and patch for diffing alghorithm

--- framework/dom.js
+++ framework/dom.js
@@ -1,7 +1,9 @@
+// this function create a virtual node element
 const addNode = (type, props = {}, children = []) => {
   return { type, props, children };
 };
 
+// this function create real dom tree from a virtual dom tree
 const createDom = (vdom) => {
     const el = document.createElement(vdom.type)
 
@@ -27,6 +29,35 @@ const createDom = (vdom) => {
     }
     return el;
 }
+
+// this function check if oldDomElement equal to newDomElement
+const changed = (oldNode, newNode) => {
+    let res = false
+    if (typeof oldNode != typeof newNode || (typeof oldNode == "string" && oldNode != newNode) || oldNode.type != newNode.type) {
+        res = true
+    }
+    if (JSON.stringify(oldNode.props) !== JSON.stringify(newNode.props)) res = true;
+    return res
+}
+
+const patch = (parent, oldNode, newNode, index = 0) => {
+    if (!oldNode) {
+        parent.appendChild(createDom(newNode))
+    } else if (!newNode) {
+        parent.removeChild(parent.childNodes[index])
+    } else if (changed(oldNode,newNode)) {
+        parent.replaceChild(createDom(newNode),parent.childNodes[index])
+    } else if (newNode.type) {
+        const oldLen = oldNode.children.length
+        const newLen = newNode.children.length
+
+        for (let i = 0; i < oldLen || i < newLen; i++) {
+            patch(parent.childNodes[index],oldNode.children[i],newNode.children[i],i)
+        }
+    }
+}
+
+
 
 const initialRender = (child, parent) => parent.appendChild(child)
 
